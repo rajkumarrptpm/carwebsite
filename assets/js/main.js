619f169e60f2901295d7e9e3070db087bf38f669
Raj kumar R <78129713+rajkumarrptpm@users.noreply.github.com>
2025-02-16 18:02:15 +0530

    image info section and responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,33 @@
 // mobile menu
 let mobileMenuIcon = document.querySelector('header .fa-bars');
 let menu = document.querySelector("header .menu")
+let ismoved = false;
+let navHeader = document.querySelector("nav-header")
 
+menu.classList.add("move-right");
 mobileMenuIcon.addEventListener('click',function (){
-    menu.classList.toggle('open'); 
+    
+    ismoved = !ismoved;
+    
+    if(ismoved){
+        menu.classList.add("open");
+        menu.classList.add("move-right");
+        // navHeader.style.overflow="display";
+        menu.classList.remove("move-left");
+        
+    }
+    else{
+        menu.classList.add("open");
+        menu.classList.add("move-left");
+        menu.classList.remove("move-right");
+        
+        
+        
+
+    }
+
 })
+
+
+
+
